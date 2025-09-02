a1211d485cc1d8cdf80db70bdbfbab90b31265ca
wsjoden <sjoden.william@gmail.com>
2025-09-02 13:33:08 +0200

    cors

--- backend/user_service/src/main/java/org/example/userservice/config/WebConfig.java
+++ backend/user_service/src/main/java/org/example/userservice/config/WebConfig.java
@@ -1,6 +1,5 @@
 package org.example.userservice.config;
 
-
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,19 +22,19 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        //config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://medical-app-frontend.app.cloud.cbh.kth.se");
+        config.addAllowedOrigin("http://localhost:3000"); // For local development
         config.addAllowedOriginPattern("*");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
-                HttpHeaders.ACCEPT
-        ));
+                HttpHeaders.ACCEPT));
         config.setAllowedMethods(Arrays.asList(
                 HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
                 HttpMethod.PUT.name(),
-                HttpMethod.DELETE.name()
-        ));
+                HttpMethod.DELETE.name(),
+                HttpMethod.OPTIONS.name()));
         config.setMaxAge(3600L);
         source.registerCorsConfiguration("/**", config);
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
@@ -49,4 +48,3 @@ public class WebConfig {
     }
 
 }
-
