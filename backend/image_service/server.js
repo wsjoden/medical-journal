e08d39306a7675f173b8043faaec80ea86231c69
wsjoden <sjoden.william@gmail.com>
2025-09-23 13:20:53 +0200

    get files

--- backend/image_service/server.js
+++ backend/image_service/server.js
@@ -178,6 +178,17 @@ app.get("/images/list", verifyJwt, checkRole(["Doctor", "Other_Staff"]), (req, r
     }
 });
 
+app.get("/images/download/:filename", verifyJwt, checkRole(["Doctor", "Other_Staff", "Patient"]), (req, res) => {
+    const filename = req.params.filename;
+    const filePath = path.join(__dirname, 'uploads', filename);
+
+    if (!fs.existsSync(filePath) || !filePath.startsWith(path.join(__dirname, 'uploads'))) {
+        return res.status(404).json({ message: "File not found" });
+    }
+
+    res.sendFile(filePath);
+});
+
 // Start Server
 const PORT = process.env.PORT || 8085;
 app.listen(PORT, () => {
