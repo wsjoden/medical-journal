830bbc46c1e5e01658f281b287098e768f13c2b1
wsjoden <sjoden.william@gmail.com>
2025-09-23 14:14:04 +0200

    blob req added

--- frontend/src/services/RESTService.js
+++ frontend/src/services/RESTService.js
@@ -86,3 +86,26 @@ export const apiRequestFile = (method, url, formData) => {
         }
     });
 };
+
+export const apiRequestBlob = (method, url) => {
+    const token = localStorage.getItem('token');
+    console.log("Token from localStorage:", localStorage.getItem('token'));
+
+    let baseURL = defaultBaseURL;
+    if (url.startsWith("/user") || url.startsWith("/patient") || url.startsWith("/staff")) {
+        baseURL = userBaseURL;
+    } else if (url.startsWith("/jwt")) {
+        baseURL = jwtBaseURL;
+    } else if (url.startsWith("/messages")) {
+        baseURL = messagesBaseURL;
+    } else if (url.startsWith("/diagnoses") || url.startsWith("/observation") || url.startsWith("/encounter")) {
+        baseURL = medicalDataBaseURL;
+    } else if (url.startsWith("/images")) {
+        baseURL = imageBaseURL;
+    } else if (url.startsWith("/search")) {
+        baseURL = searchBaseURL;
+    }
+
+    const requestURL = url.startsWith("http") ? url : baseURL + url;
+    console.log("Request URL:", requestURL);
+}
