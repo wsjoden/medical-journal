b734fc7538df8af26f360d6f9604662ab1387e88
wsjoden <sjoden.william@gmail.com>
2025-09-24 14:25:33 +0200

    cleaning up, removing email, ssn, adding staff username

--- frontend/src/components/PatientSearchBar.js
+++ frontend/src/components/PatientSearchBar.js
@@ -7,13 +7,19 @@ function PatientSearchBar({ onSearch, onAdvancedSearch }) {
     const [searchTerm, setSearchTerm] = useState('');
     const [advancedSearch, setAdvancedSearch] = useState(false);
     const [filters, setFilters] = useState({
-        staffFirstName: '',
-        staffLastName: '',
-        staffEmail: '',
-        staffSSN: '',
+        // Patient search fields
+        firstName: '',
+        lastName: '',
         diagnose: '',
         observation: '',
-        encounterDate: ''
+        encounterDate: '',
+        // Doctor & Staff search fields
+        staffFirstName: '',
+        staffLastName: '',
+        staffUserName: '',
+        // Multiple fields
+        genericSearch: ''
+
     });
 
     const handleInputChange = (e) => {
@@ -40,64 +46,63 @@ function PatientSearchBar({ onSearch, onAdvancedSearch }) {
         }
     };
 
+    const clearAdvancedFilters = () => {
+        setFilters({
+            firstName: '',
+            lastName: '',
+            diagnose: '',
+            observation: '',
+            encounterDate: '',
+            staffUserName: '',
+            staffFirstName: '',
+            staffLastName: '',
+            genericSearch: ''
+        });
+    };
+
     return (
         <div className="search-bar">
             <div className="basic-search">
                 <div className="search-input-container">
                     <input
                         type="text"
-                        name="genericSearch"
-                        placeholder="Search for patients..."
+                        name="basicSearch"
+                        placeholder="Search for patients by name..."
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
                         onKeyDown={handleKeyPress}  // Trigger search on Enter key press
                     />
                     <button onClick={handleSearch} className="search-icon">
-                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </button>
                 </div>
             </div>
+
             <button
                 onClick={() => setAdvancedSearch(!advancedSearch)}
                 className="advanced-search-button">
                 Advanced Search
             </button>
+
             {advancedSearch && (
                 <div className="advanced-search-fields">
+                    <h4>Patient Search</h4>
                     <div className="search-input-container">
                         <input
                             type="text"
-                            name="staffFirstName"
-                            placeholder="Staff first name..."
-                            value={filters.staffFirstName}
+                            name="firstName"
+                            placeholder="Patient first name..."
+                            value={filters.firstName}
                             onChange={handleInputChange}
-                            onKeyDown={handleKeyPress} // Trigger search when Enter is pressed in advanced fields
+                            onKeyDown={handleKeyPress}
                         />
                         <input
                             type="text"
-                            name="staffLastName"
-                            placeholder="Staff last name..."
-                            value={filters.staffLastName}
+                            name="lastName"
+                            placeholder="Patient last name..."
+                            value={filters.lastName}
                             onChange={handleInputChange}
-                            onKeyDown={handleKeyPress} // Trigger search when Enter is pressed in advanced fields
-                        />
-                    </div>
-                    <div className="search-input-container">
-                        <input
-                            type="text"
-                            name="staffEmail"
-                            placeholder="Staff email..."
-                            value={filters.staffEmail}
-                            onChange={handleInputChange}
-                            onKeyDown={handleKeyPress} // Trigger search when Enter is pressed in advanced fields
-                        />
-                        <input
-                            type="text"
-                            name="staffSSN"
-                            placeholder="Staff SSN..."
-                            value={filters.staffSSN}
-                            onChange={handleInputChange}
-                            onKeyDown={handleKeyPress} // Trigger search when Enter is pressed in advanced fields
+                            onKeyDown={handleKeyPress}
                         />
                     </div>
                     <div className="search-input-container">
@@ -107,27 +112,86 @@ function PatientSearchBar({ onSearch, onAdvancedSearch }) {
                             placeholder="Diagnose..."
                             value={filters.diagnose}
                             onChange={handleInputChange}
-                            onKeyDown={handleKeyPress} // Trigger search when Enter is pressed in advanced fields
+                            onKeyDown={handleKeyPress}
                         />
                         <input
                             type="text"
                             name="observation"
-                            placeholder="Observation..."
+                            placeholder="observation..."
                             value={filters.observation}
                             onChange={handleInputChange}
-                            onKeyDown={handleKeyPress} // Trigger search when Enter is pressed in advanced fields
+                            onKeyDown={handleKeyPress}
                         />
                     </div>
-                    <p className="encounter-date-label">Encounter date</p>
-                    <input
-                        type="date"
-                        name="encounterDate"
-                        placeholder="Encounter Date"
-                        value={filters.encounterDate}
-                        onChange={handleInputChange}
-                        onKeyDown={handleKeyPress} // Trigger search when Enter is pressed in advanced fields
-                        className="encounter-date-input"
-                    />
+
+                    <div>
+                        <h4>Medical Records Search</h4>
+                        <div className="search-input-container">
+                            <div className="date-input-group">
+                                <label htmlFor="encounterDate">Encounter Date:</label>
+                                <input
+                                    type="date"
+                                    name="encounterDate"
+                                    id="encounterDate"
+                                    value={filters.encounterDate}
+                                    onChange={handleInputChange}
+                                    onKeyDown={handleKeyPress}
+                                    className="encounter-date-input"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <h4>Search by Doctor/Staff</h4>
+                    <div className="search-input-container">
+                        <input
+                            type="text"
+                            name="staffUserName"
+                            placeholder='Doctor/Staff Username...'
+                            value={filters.staffUserName}
+                            onChange={handleInputChange}
+                            onKeyDown={handleKeyPress}
+                        />
+                    </div>
+                    <div className="search-input-container">
+                        <input
+                            type="text"
+                            name="staffFirstName"
+                            placeholder="Doctor/Staff first name..."
+                            value={filters.staffFirstName}
+                            onChange={handleInputChange}
+                            onKeyDown={handleKeyPress}
+                        />
+                        <input
+                            type="text"
+                            name="staffLastName"
+                            placeholder="Doctor/Staff last name..."
+                            value={filters.staffLastName}
+                            onChange={handleInputChange}
+                            onKeyDown={handleKeyPress}
+                        />
+                    </div>
+
+                    <h4>Generic Search</h4>
+                    <div className="search-input-container">
+                        <input
+                            type="text"
+                            name="genericSearch"
+                            placeholder="Search across all fields..."
+                            value={filters.genericSearch}
+                            onChange={handleInputChange}
+                            onKeyDown={handleKeyPress}
+                        />
+                    </div>
+
+                    <div className="search-buttons">
+                        <button onClick={handleSearch} className="search-button">
+                            Search
+                        </button>
+                        <button onClick={clearAdvancedFilters} className="clear-button">
+                            Clear Filters
+                        </button>
+                    </div>
                 </div>
             )}
         </div>
